da66b2dcfa790edaecf1f74e10dc3904ef8c9931
Admin <admin@mac-admin.local>
2015-12-28 14:00:15 +0200

    Initial Commit

---
+++ ButtonsHomework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonsHomework
+//
+//  Created by Admin on 28.12.15.
+//  Copyright © 2015 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
