3a800560c8e3431962735b16bae08448939613d4
Alina <dizialina@gmail.com>
2016-01-22 20:45:54 +0200

    first commit

--- ButtonsHomework/ViewController.h
+++ ButtonsHomework/ViewController.h
@@ -10,6 +10,36 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *zeroButton;
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
+@property (weak, nonatomic) IBOutlet UIButton *fiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *sixButton;
+@property (weak, nonatomic) IBOutlet UIButton *sevenButton;
+@property (weak, nonatomic) IBOutlet UIButton *eightButton;
+@property (weak, nonatomic) IBOutlet UIButton *nineButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *divineButton;
+@property (weak, nonatomic) IBOutlet UIButton *multipleButton;
+@property (weak, nonatomic) IBOutlet UIButton *minusButton;
+@property (weak, nonatomic) IBOutlet UIButton *plusButton;
+@property (weak, nonatomic) IBOutlet UIButton *resultButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+@property (weak, nonatomic) IBOutlet UIButton *dotButton;
+@property (weak, nonatomic) IBOutlet UIButton *reverseButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *monitorLabel;
+
+- (IBAction) numberButtonAction:(UIButton*) sender;
+- (IBAction) resetButtonAction:(UIButton *) sender;
+- (IBAction) doButtonAction:(UIButton *) sender;
+- (IBAction) resultAction:(UIButton *) sender;
+- (IBAction) reverseAction:(UIButton *) sender;
+- (IBAction) dotAction:(UIButton *) sender;
+
 
 @end
 
--- ButtonsHomework/ViewController.m
+++ ButtonsHomework/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    
+    NSMutableString* currentNumber;
+    NSString* operator;
+    NSString* firstNumber;
+    
+}
 
 @end
 
@@ -17,11 +23,180 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    
+    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background.jpg"]];
+    
+    [self.view addSubview:backgroundView];
+    
+    NSArray* allButtonsArray = [[NSArray alloc] initWithObjects:self.zeroButton, self.oneButton, self.twoButton, self.threeButton, self.fourButton, self.fiveButton, self.sixButton, self.sevenButton, self.eightButton, self.nineButton, self.multipleButton, self.divineButton, self.plusButton, self.minusButton, self.resultButton, self.resetButton, self.dotButton, self.reverseButton, nil];
+    
+    [self setButtonsAttrinutes:allButtonsArray];
+    
+    currentNumber = [[NSMutableString alloc] init];
+    
+    [currentNumber setString:@"0"];
+    
+    firstNumber = nil;
+    
+        
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Private Methods
+
+- (IBAction) numberButtonAction:(UIButton*) sender {
+    
+    if (currentNumber.length < 12) {
+    
+        if ([sender.currentTitle isEqualToString:@"0"]) {
+        
+            if (![currentNumber isEqualToString:@"0"]) {
+            
+                [currentNumber appendString:sender.currentTitle];
+            
+                self.monitorLabel.text = currentNumber;
+            
+            }
+        
+        } else {
+        
+            if ([currentNumber isEqualToString:@"0"]) {
+            
+                [currentNumber setString:@""];
+    
+                [currentNumber appendString:sender.currentTitle];
+    
+                self.monitorLabel.text = currentNumber; }
+        
+            else {
+            
+                [currentNumber appendString:sender.currentTitle];
+            
+                self.monitorLabel.text = currentNumber;
+            }
+        
+        }
+    }
+    
+}
+
+- (IBAction) dotAction:(UIButton *) sender {
+    
+    [currentNumber appendString:sender.currentTitle];
+    
+    self.monitorLabel.text = currentNumber;
+    
+}
+
+- (IBAction)resetButtonAction:(UIButton *)sender {
+    
+    self.monitorLabel.text = @"0";
+    
+    [currentNumber setString:@"0"];
+    
+}
+
+- (void) setButtonsAttrinutes:(NSArray*) arrayButtons {
+    
+    for (UIButton* button in arrayButtons) {
+        
+        button.layer.borderColor = [[UIColor whiteColor] CGColor];
+        button.layer.borderWidth = 0.8f;
+        button.layer.cornerRadius = 38.f;
+        
+    }
+                                 
+    
+}
+
+- (IBAction) doButtonAction:(UIButton *) sender {
+    
+    if (firstNumber != nil) {
+        
+        [self resultAction:sender];
+        
+    }
+    
+    operator = sender.currentTitle;
+    
+    firstNumber = [currentNumber copy];
+    
+    [currentNumber setString:@""];
+    
+    
+    
+}
+
+- (IBAction) resultAction:(UIButton *) sender {
+    
+    if ([operator isEqualToString:@"+"]) {
+        
+        [self didTerminateOperator:([firstNumber floatValue] + [currentNumber floatValue])];
+        
+    }
+    
+    if ([operator isEqualToString:@"-"]) {
+        
+        [self didTerminateOperator:([firstNumber floatValue] - [currentNumber floatValue])];
+        
+    }
+    
+    if ([operator isEqualToString:@"x"]) {
+        
+        [self didTerminateOperator:([firstNumber floatValue] * [currentNumber floatValue])];
+        
+    }
+    
+    if ([operator isEqualToString:@"÷"]) {
+        
+        [self didTerminateOperator:([firstNumber floatValue] / [currentNumber floatValue])];
+        
+    }
+    
+    firstNumber = nil;
+    
+}
+
+- (void) didTerminateOperator:(CGFloat) result {
+    
+    [currentNumber setString:[NSString stringWithFormat:@"%g", result]];
+    
+    self.monitorLabel.text = currentNumber;
+    
+}
+
+- (IBAction) reverseAction:(UIButton *) sender {
+    
+    if (![self.monitorLabel.text isEqualToString:@"0"]) {
+    
+        float temp = [currentNumber floatValue] * (-1);
+    
+        NSString* reverseResult = [NSString stringWithFormat:@"%g", temp];
+    
+        [currentNumber setString:reverseResult];
+    
+        self.monitorLabel.text = currentNumber;
+        
+    } else {
+        
+        self.monitorLabel.text = @"0";
+        
+    }
+    
+}
+
 
 @end
